ce7e536775836a4e591206f3b534e158d02a7b09
rdurbano <rafael.durbanoneto@gmail.com>
2022-01-23 19:40:09 -0300

    - Correcao do visual miolo - Remocao de css nao utilizados - Add Sub Menu - Add Nome colunas teste

--- Metadados/Metadados/Models/Objeto.cs
+++ Metadados/Metadados/Models/Objeto.cs
@@ -6,20 +6,35 @@ namespace Metadados.Models
     public class Objeto
     {
         [Key]
+        [Display(Name ="ID")]
         public int sky_objeto { get; set; }
         [ForeignKey("objeto_configuracao")]
+
+        [Display(Name = "ID Objeto Configuração")]
         public int sky_objeto_configuracao { get; set; }
         [ForeignKey ("objeto_localizacao")]
+
+        [Display(Name = "ID Objeto Localização")]
         public int sky_objeto_localizacao { get; set; }
         [Column(TypeName = "varchar(250)")]
+
+        [Display(Name = "Nome Objeto")]
         public string nom_objeto { get; set; }
         [Column(TypeName = "varchar(250)")]
+
+        [Display(Name = "Compactação")]
         public string nom_compactacao { get; set; }
         [Column(TypeName = "varchar(250)")]
+
+        [Display(Name = "Nome Tabela")]
         public string nom_tabela { get; set; }
         [Column(TypeName = "varchar(250)")]
+
+        [Display(Name = "Nome Entidade")]
         public string nom_entidade { get; set; }
         [Column(TypeName = "varchar(250)")]
+
+        [Display(Name = "Range de colunas")]
         public string dsc_coluna_ranges { get; set; }
 
     }
